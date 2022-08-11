30d89b7b6bd7d21cf7adc7c84dccf80eb62a4a92
gokulhansv <gokulhansv@gmail.com>
2022-08-11 07:35:06 +0530

    new

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
   const [codebook, setCodebook] = useState([])
 
   useEffect(() => {
-    axios.get('http://localhost:5000/getcodes').then(res => {
+    axios.get('/getcodes').then(res => {
       console.log(res.data);
       setCodebook(res.data)
     })

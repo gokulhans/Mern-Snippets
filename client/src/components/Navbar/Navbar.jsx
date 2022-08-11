295abeddcf086e0be94ad9a9163947e812ff1a4e
gokulhansv <gokulhansv@gmail.com>
2022-08-11 20:48:33 +0530

    new

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import React from 'react'
 import '../../App.css'
-
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
@@ -22,10 +21,10 @@ function AppNavbar() {
               navbarScroll
             >
               <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/login">Login</Nav.Link>
-              <Nav.Link as={Link} to="/register">Register</Nav.Link>
+              {/* <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              <Nav.Link as={Link} to="/register">Register</Nav.Link> */}
               <Nav.Link as={Link} to="/upload">Upload</Nav.Link>
-              <Nav.Link as={Link} to="/editcode">Editcode</Nav.Link>
+              {/* <Nav.Link as={Link} to="/editcode">Editcode</Nav.Link> */}
               <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
               <Nav.Link as={Link} to="/codegen">CodeGen</Nav.Link>
              
